2091538b3b3a25dcad36ffb523690adff1f5b03b
lucas <lucas.petit@etu.univ-lyon1.fr>
2023-09-13 21:54:08 +0200

    Initial Commit

---
+++ AppTinyMesh/Include/qte.h
@@ -0,0 +1,36 @@
+#ifndef __Qte__
+#define __Qte__
+
+#include <QtWidgets/qmainwindow.h>
+#include "realtime.h"
+#include "meshcolor.h"
+
+QT_BEGIN_NAMESPACE
+	namespace Ui { class Assets; }
+QT_END_NAMESPACE
+
+class MainWindow : public QMainWindow
+{
+  Q_OBJECT
+private:
+  Ui::Assets* uiw;           //!< Interface
+
+  MeshWidget* meshWidget;   //!< Viewer
+  MeshColor meshColor;		//!< Mesh.
+
+public:
+  MainWindow();
+  ~MainWindow();
+  void CreateActions();
+  void UpdateGeometry();
+
+public slots:
+  void editingSceneLeft(const Ray&);
+  void editingSceneRight(const Ray&);
+  void BoxMeshExample();
+  void SphereImplicitExample();
+  void ResetCamera();
+  void UpdateMaterial();
+};
+
+#endif
